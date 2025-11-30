52c3c9f506ae881dd5fc2d2abfc82c68324f52a5
janing06 <jhonmichaelturija@gmail.com>
2025-11-30 21:53:03 +0800

    chore: add fsd folder structure, install steiger and knipk

--- client/src/app/App.tsx
+++ client/src/app/App.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import reactLogo from '@app/assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
@@ -33,3 +33,4 @@ function App() {
 }
 
 export default App
+ 
--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import path from 'path';
 
-// https://vite.dev/config/
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      '@app': path.resolve(__dirname, './src/app'),
+      '@entities': path.resolve(__dirname, './src/entities'),
+      '@features': path.resolve(__dirname, './src/features'),
+      '@pages': path.resolve(__dirname, './src/pages'),
+      '@shared': path.resolve(__dirname, './src/shared'),
+      '@widgets': path.resolve(__dirname, './src/widgets'),
+    },
+  },
+});
